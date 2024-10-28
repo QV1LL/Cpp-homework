0d21e560115cdd87536c3bf18ebb10ce7f23e07b
QV1LL <ghost228678@gmail.com>
2024-10-28 17:42:22 +0100

    Done homework 15

--- Homework15 (Structs)/library.h
+++ Homework15 (Structs)/library.h
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+auto isFirstStringBiggerByAlphabet(string text1, string text2) -> bool;
+
 struct Book {
 private:
 	string title;
@@ -13,32 +15,38 @@ private:
 
 public:
 	Book() {
-
+		this->title = "???";
+		this->author = "???";
+		this->publisher = "???";
 	}
 
-	Book(string title, string author, string publisher) {
-		this->title = title;
-		this->author = author;
-		this->publisher = publisher;
-	}
+Book(string title, string author, string publisher) {
+	this->title = title;
+	this->author = author;
+	this->publisher = publisher;
+}
 
-	auto setStatements(string title, string author, string publisher) -> void {
-		this->title = title;
-		this->author = author;
-		this->publisher = publisher;
-	}
+auto setStatements(string title, string author, string publisher) -> void {
+	this->title = title;
+	this->author = author;
+	this->publisher = publisher;
+}
 
-	auto getTitle() -> string { return this->title; }
-	auto getAuthor() -> string { return this->author; }
-	auto getPublisher() -> string { return this->publisher; }
+auto getTitle() -> string { return this->title; }
+auto getAuthor() -> string { return this->author; }
+auto getPublisher() -> string { return this->publisher; }
 
-	friend struct Library;
+auto setTitle(string title) -> void { this->title = title; }
+auto setAuthor(string author) -> void { this->author = author; }
+auto setPublisher(string publisher) -> void { this->publisher = publisher; }
+
+friend struct Library;
 };
 
 struct Library {
 private:
-		Book* books;
-		uint16_t booksCount;
+	Book* books;
+	uint16_t booksCount;
 
 public:
 	Library() {
@@ -46,28 +54,22 @@ public:
 		this->booksCount = 10;
 	}
 
-	Library(int booksCount) {
+	Library(uint16_t booksCount) {
 		this->books = new Book[booksCount];
 		this->booksCount = booksCount;
 	}
 
-	Library(int booksCount, Book* books) {
+	Library(uint16_t booksCount, Book* books) {
 		this->books = books;
 		this->booksCount = booksCount;
 	}
 
-	auto getBook(int id) -> Book {
-		return this->books[id];
+	auto getBook(int id) -> Book* {
+		return &(this->books[id]);
 	}
 
 	auto getBooksCount() -> int {
 		return this->booksCount;
-	}
-
-	auto setBook(Book& book, int id) -> void {
-		delete &(this->books[id]);
-
-		this->books[id] = book;
 	}
 
 	auto findBookByAuthor(string author, int entrance) -> Book* {
@@ -98,5 +100,61 @@ public:
 		}
 
 		return nullptr;
+	}
+
+	auto printBooks() -> void {
+		for (int i = 0; i < this->booksCount; i++) {
+			Book currentBook = this->books[i];
+
+			cout << "BOOK " << i + 1 << endl
+				<< "Title \"" << currentBook.title << "\"" << endl
+				<< "Author " << currentBook.author << endl
+				<< "Publisher " << currentBook.publisher << endl;
+		}
+	}
+
+	auto sortBooksByTitle() -> void {
+		for (int i = 0; i < this->booksCount; i++) {
+			for (int j = 0; j < this->booksCount; j++) {
+				Book* book1 = &(this->books[i]);
+				Book* book2 = &(this->books[j]);
+
+				if (isFirstStringBiggerByAlphabet(book1->title, book2->title)) {
+					auto pTemp = *book1;
+					*book1 = *book2;
+					*book2 = pTemp;
+				}
+			}
+		}
+	}
+
+	auto sortBooksByAuthor() -> void {
+		for (int i = 0; i < this->booksCount; i++) {
+			for (int j = 0; j < this->booksCount; j++) {
+				Book* book1 = &(this->books[i]);
+				Book* book2 = &(this->books[j]);
+
+				if (isFirstStringBiggerByAlphabet(book1->author, book2->author)) {
+					auto pTemp = *book1;
+					*book1 = *book2;
+					*book2 = pTemp;
+				}
+			}
+		}
+	}
+
+	auto sortBooksByPublisher() -> void {
+		for (int i = 0; i < this->booksCount; i++) {
+			for (int j = 0; j < this->booksCount; j++) {
+				Book* book1 = &(this->books[i]);
+				Book* book2 = &(this->books[j]);
+
+				if (isFirstStringBiggerByAlphabet(book1->publisher, book2->publisher)) {
+					auto pTemp = *book1;
+					*book1 = *book2;
+					*book2 = pTemp;
+				}
+			}
+		}
 	}
 };
--- Homework15 (Structs)/main.cpp
+++ Homework15 (Structs)/main.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
+#include "library.h"
+#include <time.h>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+static const char alphanum[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+auto genRandomString(int stringLength) -> string
+{
+	string result;
+
+	for (int i = 0; i < stringLength; i++)
+		result += alphanum[rand() % stringLength];
+
+	return result;
+}
+
+auto isFirstStringBiggerByAlphabet(string text1, string text2) -> bool {
+	if (text1 == text2)
+		return false;
+
+	for (int i = 0; i < text1.size(); i++) {
+		int text1Element = text1[i];
+		int text2Element = text2[i];
+
+		if ((48 <= text1Element && text1Element <= 57) || (65 <= text1Element && text1Element <= 90))
+			text1Element += 32;
+		else if (97 <= text1Element && text1Element <= 122) {
+
+		}
+		else {
+			text1Element += 256;
+		}
+
+		if ((48 <= text2Element && text2Element <= 57) || (65 <= text2Element && text2Element <= 90))
+			text2Element += 32;
+		else if (97 <= text2Element && text2Element <= 122) {
+
+		}
+		else {
+			text1Element += 256;
+		}
+
+		if (text1Element < text2Element)
+			return true;
+		else if (text1Element > text2Element)
+			return false;
+	}
+
+	return false;
+}
+
 int main () {
+	srand(time(0));
+
+	Library* library = new Library();
+
+	for (int i = 0; i < library->getBooksCount(); i++) {
+		Book* book = library->getBook(i);
+
+		book->setStatements(genRandomString(8), genRandomString(8), genRandomString(8));
+	}
+
+	library->sortBooksByTitle();
+
+	library->printBooks();
 
 	return 0;
 }
